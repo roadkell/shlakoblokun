5708b1aa37622f882c6024f7c59dcc2da4294d55
Kelly Roadkill <4406611+roadkell@users.noreply.github.com>
2022-08-01 15:43:30 +0500

    refactor: separate logic from presentation a bit

--- shlakoblokun.py
+++ shlakoblokun.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 """
-Generate hilarious (or not) portmanteaus from a vocabulary
+Generate hilarious (or not) portmanteaus from a vocabulary.
 
 Each word is checked for overlapping characters against every other word.
 It is considered a match when at least some chars are overlapping.
 Example: "revenge" and "vengeance" have 5 overlapping chars,
 so "reVENGEance" will be generated from this pair.
 Both words must have some non-overlapping chars at the start/end.
+
+Arbitrary decision:
+	When a blend is generated, we take overlapping chars from the second word.
+	This might influence how the resulting blend is displayed, when
+	there are uppercased/compound chars like ß (we use str.casefold()
+	for all comparisons, so matching substrings might actually look different).
+
+Naming conventions:
+	...s    - suffix for plurals (sequence)
+	...ss   - suffix for 'sequence of 'sequence''
+	w       - word (ww, ws - words)
+	w...    - prefix for word
+	cfw     - casefolded word
+	cf...   - prefix for casefolded
+	ch      - character
+	f       - file (ff, fs - files)
+	p       - path (pp, ps - paths)
+
+It might be slightly confusing with multidimensional stuff, for example:
+	wlist   - list of word (i.e. a sequence itself, could also be 'ws' or 'ww');
+	wlists  - sequence of wlist (i.e. a sequence of list of word,
+	          2-dimensional sequence, could also be 'wss' or 'wws' or 'www');
+	          and since word is a str (a sequence itself), its chars may be
+	          addressed as 'chsss' (3-dimensional sequence);
+	          we don't go that deep though. =)
 """
 # TODO: make a clean way of exiting infinite mode
 # TODO: separate logic from presentation
@@ -44,26 +69,27 @@ def main() -> int:
 
 	print('Loading vocabulary...', end=' ')
 
-	words = [[], []]
-	(words[0], words[1]) = read_infiles(args.infile, args.w1, args.w2)
+	wlists = [[], []]
+	(wlists[0], wlists[1]) = read_infiles(args.infile, args.w1, args.w2)
 
-	for (i, ws) in enumerate(words):
-		words[i] = filter_words(ws,
-		                        args.randomize,
-		                        args.length,
-		                        args.capitalized,
-		                        args.phrases)
+	for (i, wlist) in enumerate(wlists):
+		wlists[i] = filter_words(wlist,
+		                         args.randomize,
+		                         args.minlength,
+		                         args.capitalized,
+		                         args.phrases)
 
 	print('Done.')
-	print(len(words[0]) + len(words[1]), 'words loaded,',
-	      len(words[0]) * len(words[1]), 'pairs to check.')
+	print(len(wlists[0]) + len(wlists[1]), 'words loaded,',
+	      len(wlists[0]) * len(wlists[1]), 'pairs to check.')
 	print('Starting search for overlapping substrings in word pairs...')
 
 	numblends = write_outfile(args.outfile,
-	                          words,
-	                          cachelist,
+	                          wlists,
+	                          # cachelist,
 	                          args.number,
 	                          args.depth,
+	                          args.minfree,
 	                          args.uppercase)
 
 	# write_cache(cachepath, cachelist)
@@ -77,7 +103,7 @@ def main() -> int:
 
 def parse_args() -> argparse.Namespace:
 	"""
-	Parse command line arguments
+	Parse command line arguments. Print help when invoked without args.
 	"""
 	parser = argparse.ArgumentParser()
 
@@ -107,10 +133,18 @@ def parse_args() -> argparse.Namespace:
 	                    type=int,
 	                    default=2,
 	                    help='minimum depth of blending (default: %(default)s)')
-	parser.add_argument('-l', '--length',
+	parser.add_argument('--minfree',
+	                    type=int,
+	                    default=1,
+	                    help='minimum number of non-overlapping chars in each word (default: %(default)s)')
+	parser.add_argument('--minlength',
 	                    type=int,
 	                    default=3,
 	                    help='minimum length of source words (default: %(default)s)')
+	parser.add_argument('--maxlength',
+	                    type=int,
+	                    default=0,
+	                    help='maximum length of source words (default: unlimited)')
 	parser.add_argument('-u', '--uppercase',
 	                    action='store_true',
 	                    help='uppercase overlapping characters in the output ("reVENGEance")')
@@ -131,9 +165,9 @@ def parse_args() -> argparse.Namespace:
 # ============================================================================ #
 
 
-def read_cache(cpath: Path) -> list[namedtuple]:
+def read_cache(cpath: Path) -> list:
 	"""
-	Read cache file into a list of CacheEntries
+	Read cache file into a list of CacheEntries.
 	"""
 	# TODO: use CSV
 	clist = []
@@ -146,9 +180,9 @@ def read_cache(cpath: Path) -> list[namedtuple]:
 # ============================================================================ #
 
 
-def write_cache(cpath: Path, clist: list[namedtuple]):
+def write_cache(cpath: Path, clist: list):
 	"""
-	Write list of CacheEntries back into cache file
+	Write list of CacheEntries back into cache file.
 	"""
 	# TODO: use CSV
 	with cpath.open(mode='r+') as f:
@@ -161,34 +195,33 @@ def write_cache(cpath: Path, clist: list[namedtuple]):
 
 def read_infiles(*pathstrs) -> tuple[list, list]:
 	"""
-	Get list of filenames for vocabulary file[s] and read their content
+	Get list of filenames for vocabulary file[s] and read their content.
 	"""
-	pathsets = [set(), set(), set()]    # common, w1, w2
-	for (i, pp) in enumerate(pathstrs):
-		if type(pp) == list:
-			for p in pp:
-				pathsets[i] |= pathstr2pathset(p)
-		elif type(pp) == str:
-			pathsets[i] = pathstr2pathset(pp)
-		# else:
-			# print(str(pp) + ' is not a valid path, skipping...')
+	pathsets = [set(), set(), set()]    # [common, w1, w2]
+	for (pathset, pathstr) in zip(pathsets, pathstrs):
+		if type(pathstr) == list:
+			for path in pathstr:
+				pathset |= pathstr2pathset(path)
+		elif type(pathstr) == str:
+			pathset = pathstr2pathset(pathstr)
 
-	wordsets = [set(), set(), set()]
-	for (i, ps) in enumerate(pathsets):
-		for p in ps:
-			wordsets[i] |= set(file2list(p))
+	wsets = [set(), set(), set()]    # [common, w1, w2]
+	for (wset, pathset) in zip(wsets, pathsets):
+		for path in pathset:
+			wset |= set(file2list(path))
 
-	w1s = sorted(wordsets[0] | wordsets[1])
-	w2s = sorted(wordsets[0] | wordsets[2])
+	wlists = [[], []]
+	wlists[0] = sorted(wsets[0] | wsets[1])
+	wlists[1] = sorted(wsets[0] | wsets[2])
 
-	return (w1s, w2s)
+	return (wlists[0], wlists[1])
 
 # ============================================================================ #
 
 
 def pathstr2pathset(pathstr: str) -> set[Path]:
 	"""
-	Unwrap a given nonempty path string into a set of absolute paths
+	Unwrap a given nonempty path string into a set of absolute file paths.
 	"""
 	paths = set()
 	if pathstr:
@@ -212,7 +245,7 @@ def pathstr2pathset(pathstr: str) -> set[Path]:
 
 def file2list(path: Path) -> list[str]:
 	"""
-	Import words from a single vocabulary file into a list
+	Import words from a single vocabulary file into a list.
 	"""
 	words = []
 	with path.open() as f:
@@ -234,15 +267,15 @@ def file2list(path: Path) -> list[str]:
 
 def filter_words(words: list,
                  do_randomize: bool,
-                 wlen_min: int,
+                 minlen: int,
                  incl_capitalized: bool,
                  incl_phrases: bool) -> list[str]:
 	"""
-	Filter word list according to given arguments
+	Filter word list according to given arguments.
 	"""
 	outwords = []
 	for w in words:
-		if (len(w) >= wlen_min) \
+		if (len(w) >= minlen) \
 		   and (incl_capitalized or w.islower()) \
 		   and (incl_phrases or (' ' not in w)):
 			outwords.append(w)
@@ -256,16 +289,20 @@ def filter_words(words: list,
 
 
 def write_outfile(outfile,
-                  words: list[list[str]],
-                  cachelist: namedtuple,
+                  wlists: list[list[str]],
+                  # cachelist,
                   maxblends: int,
                   mindepth: int,
+                  minfree: int,
                   uppercase: bool) -> int:
 	"""
 	Search for overlapping characters at the start & end of all words,
 	blend matching pairs, and write results into a text file.
+
 	Dictionary is used to save the number of overlapping chars (as values)
 	and to auto-deduplicate saved words (as keys).
+	Note: we use w.casefold() for all string comparisons, but then
+	we blend original words for output.
 	"""
 	with outfile:
 
@@ -282,44 +319,54 @@ def write_outfile(outfile,
 			                  unit='w',
 			                  desc='Word blends generated')
 
-		w1_pbar = tqdm(words[0],
-		               smoothing=0.01,
-		               dynamic_ncols=True,
-		               unit='w',
-		               desc='First words processed')
+		w_pbar = tqdm(wlists[0],
+		              smoothing=0.01,
+		              dynamic_ncols=True,
+		              unit='w',
+		              desc='First words processed')
 
 		blends = dict()
 		blend_ctr = 0
-		w1_ctr = 0
+		w_ctr = 0
+		words = ['', '']
 
-		while w1_ctr < len(words[0]) and (maxblends <= 0 or blend_ctr < maxblends):
+		while w_ctr < len(wlists[0]) and (maxblends <= 0 or blend_ctr < maxblends):
 
-			w1 = words[0][w1_ctr]
+			words[0] = wlists[0][w_ctr]
 
-			for w2 in tqdm(words[1],
-			               leave=False,
-			               dynamic_ncols=True,
-			               unit='w',
-			               desc='Current word vs. whole vocabulary'):
+			for words[1] in tqdm(wlists[1],
+			                     leave=False,
+			                     dynamic_ncols=True,
+			                     unit='w',
+			                     desc='Current word vs. whole vocabulary'):
 
-				(blend, depth) = check_n_blend(w1, w2, mindepth, uppercase)
-				if blend \
-				   and (blend not in (words[0] + words[1])) \
-				   and (blend.lower() not in (words[0] + words[1])) \
-				   and (blend not in blends):
-					blends[blend] = depth
-					# Dynamically write blended word into plain text file
-					# together with overlap depth (\n is auto-appended)
-					print(depth, blend, file=outfile)
-					blend_ctr += 1
-					blend_pbar.update()
-					if blend_ctr >= maxblends:
-						break
+				(startpos, depth), _, _ = check_pair((words[0], words[1]),
+				                                     mindepth,
+				                                     minfree)
+				if depth:
+					blend = blend_pair((words[0], words[1]),
+					                   startpos,
+					                   depth,
+					                   uppercase)
 
-			w1_ctr += 1
-			w1_pbar.update()
+					if (blend not in (wlists[0] + wlists[1])) \
+					   and (blend.lower() not in (wlists[0] + wlists[1])) \
+					   and (blend not in blends):
+						blends[blend] = depth
+						# Dynamically write blended word into plain text file
+						# together with overlap depth (\n is auto-appended)
+						# TODO: argumentize output string format
+						# TODO: also save all data into cache
+						print(blend, file=outfile)
+						blend_ctr += 1
+						blend_pbar.update()
+						if blend_ctr >= maxblends:
+							break
 
-		w1_pbar.close()
+			w_ctr += 1
+			w_pbar.update()
+
+		w_pbar.close()
 		blend_pbar.close()
 
 	return blend_ctr
@@ -327,33 +374,103 @@ def write_outfile(outfile,
 # ============================================================================ #
 
 
-def check_n_blend(w1: str,
-                  w2: str,
-                  mindepth: int,
-                  uppercase: bool) -> tuple[str, int]:
+def check_pair(ws: tuple[str, str],
+               mindepth: int,
+               minfree: int) -> tuple[tuple[int, int],
+                                      tuple[str, str],
+                                      tuple[int, int]]:
 	"""
-	Check a pair of words for overlapping characters.
-	There must also be some non-overlapping characters in both words.
-	If match is found, blend them.
-	"""
-	wblend = ''
-	depth = 0
-	# if w1 != w2:
-	for i in range(1, len(w1) - (mindepth-1)):
-		if w2.casefold().startswith(w1.casefold()[i:], 0, len(w2) - 1):
-			# Match! Now blend w1 & w2:
-			# take i non-overlapping chars from w1,
-			# add overlapping chars(optionally UPPERCASE them),
-			# then add remaining chars from w2.
-			depth = len(w1[i:])
-			if uppercase:
-				wblend = ''.join((w1[:i], w1[i:].upper(), w2[depth:]))
-			else:
-				wblend = ''.join((w1, w2[depth:]))
-			# After a match is found, exit for loop (this pair is done)
-			break
+	Check word pair for blendability.
 
-	return (wblend, depth)
+	Returns a tuple of:
+		- tuple (startpos, depth)
+		- tuple (w[0].casefold(), w[1].casefold())
+		- tuple (cfstartpos, cfdepth)
+	Note: this algorithm uses casefold() string comparison, which, for some
+	chars (like ß), changes word length (and, consequently, char indices).
+	Even more, for some languages unicodedata.normalize('NFKD', str)
+	might be needed. For now, we only address the first case,
+	trying to calculate proper startpos & depth.
+	https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
+	"""
+	cfws = []
+	for w in ws:
+		cfws.append(w.casefold())
+
+	for i in range(minfree, len(cfws[0]) - (mindepth-1)):
+
+		if cfws[1].startswith(cfws[0][i:], 0, len(cfws[1]) - minfree):
+			# Match!
+			cfstartpos = i
+			cfdepth = len(cfws[0][i:])
+
+			# Check if w.casefold differs from w, and calculate
+			# proper startpos & depth for non-casefolded w
+			# TODO: make a for loop for ws[0..1] instead
+			if len(ws[0]) != len(cfws[0]):
+				startpos = 0
+				chlensum = 0
+				for (i, ch) in enumerate(ws[0]):
+					chlensum += len(ch.casefold())
+					if chlensum >= cfstartpos:
+						startpos = i + 1    # may or may not be = cfstartpos
+						break
+					# elif chlensum > cfstartpos:
+						# a complex case when blend starts inside a compound
+						# letter (e.g., Buße / busSE + SEmantic), we'll deal
+						# with it later
+			else:
+				# Equality for w[0] means both startpos & depth are correct
+				# (depth for original w[1] might differ, though)
+				startpos = cfstartpos
+
+			if len(ws[1]) != len(cfws[1]):
+				depth = 0
+				chlensum = 0
+				for (i, ch) in enumerate(ws[1]):
+					chlensum += len(ch.casefold())
+					if chlensum >= cfdepth:
+						depth = i + 1       # may or may not be = cfdepth
+						break
+					# elif chlensum > cfdepth:
+						# a complex case when blend ends inside a compound
+						# letter, we'll deal with it later
+			else:
+				# Equality for w[1] means depth is correct
+				# (depth for original w[0] might differ, though)
+				depth = cfdepth
+
+			# After a match is found, exit for loop (this pair is done)
+			return ((startpos, depth),
+			        (cfws[0], cfws[1]),
+			        (cfstartpos, cfdepth))
+
+	return ((0, 0), ('', ''), (0, 0))
+
+# ============================================================================ #
+
+
+def blend_pair(ws: tuple[str, str],
+               startpos: int,
+               depth: int,
+               uppercase: bool) -> str:
+	"""
+	Blend word pair.
+
+	Take 'startpos' non-overlapping chars from w[0],
+	add 'depth' overlapping chars (optionally UPPERCASE them),
+	then add remaining chars from w[1].
+	Arbitrary decision: take overlapping chars from w[1]. If there are
+	differently uppercased/compound chars (like ß), result might look different.
+	"""
+	if uppercase:
+		blend = ''.join((ws[0][:startpos], ws[1][:depth].upper(), ws[1][depth:]))
+		# To use overlapping chars from ws[0] instead (maybe argumentize this):
+		# blend = ''.join((ws[0][:startpos], ws[0][startpos:].upper(), ws[1][depth:]))
+	else:
+		blend = ''.join((ws[0][:startpos], ws[1]))
+
+	return blend
 
 # ============================================================================ #
 
