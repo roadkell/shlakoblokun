d5f68adc9ab7ef4cb9f41bfe1e22f764e3e77057
Kelly Roadkill <4406611+roadkell@users.noreply.github.com>
2023-10-18 21:20:59 +0500

    style: :art: update paths, reindent, reword description, fix shebang

--- shlakoblokun-ru.sh
+++ shlakoblokun-ru.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#
 # Shlakoblokun launch script with some options given as an example.
-# It generates random blends, using vocabularies from shlakoblokun/data/ru/
-# folder, and saves the result into output-ru.txt.
+# https://github.com/roadkell/shlakoblokun
+#
+# Generate random blends, using vocabularies from shlakoblokun/data/ru/ folder,
+# and save the result into output-ru.txt.
 # You can run the script as is, or modify the options to your liking.
 
 cd shlakoblokun/ || exit
-python3 shlakoblokun.py -i data/ru/n.txt \
-						-w2 data/ru/adj.txt data/ru/adv.txt data/ru/v.txt \
-						-e data/ru/.exclude-overlaps.txt \
-						-o ../output-ru.txt \
-						-d 3 -r -u
+python3 shlakoblokun.py \
+	-i data/ru/n.txt \
+	-b data/ru/adj.txt data/ru/adv.txt data/ru/v.txt \
+	-e data/ru/overlaps.txt \
+	-o ../output-ru.txt \
+	-d 3 -r -u
